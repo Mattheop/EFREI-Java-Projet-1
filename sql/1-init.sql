a01d3e3e88277254d7b0f3fa762a638b09deb038
Matthéo PERELLE <mattheo.perelle@gmail.com>
2023-09-27 15:16:25 +0200

    feat(docker): add docker-compose and database schema

---
+++ sql/1-init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS employee
+(
+    id         INTEGER PRIMARY KEY AUTO_INCREMENT,
+    type       ENUM ('developer', 'other') NOT NULL DEFAULT ('other'),
+    first_name VARCHAR(30)                 NOT NULL,
+    last_name  VARCHAR(30)                 NOT NULL,
+    address    VARCHAR(100)                NOT NULL,
+    nickname   VARCHAR(30)                 NOT NULL,
+    chief      INTEGER                              DEFAULT NULL,
+    hobbies    VARCHAR(255)                         DEFAULT NULL,
+    birth_year INT                         NOT NULL,
+    salary     FLOAT                       NOT NULL,
+    bonus      FLOAT                       NOT NULL,
+
+    CONSTRAINT ck_salary_positive CHECK ( salary > 0 ),
+    CONSTRAINT ck_bonus_positive CHECK ( bonus > 0 ),
+    CONSTRAINT ck_birth_year CHECK ( birth_year > 0 ),
+    CONSTRAINT fk_chief FOREIGN KEY (chief) REFERENCES employee (id) ON DELETE SET NULL
+
+);
