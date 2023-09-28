bf4b71fc25837b23de960936c4f8d74be88f2d2c
aroullotge1 <aristote.roullot@universite-paris-saclay.fr>
2023-09-28 09:22:45 +0200

    Ajout du Mapper Employee

---
+++ src/mapper/EmployeeMapper.java
@@ -0,0 +1,42 @@
+package mapper;
+
+import model.Employee;
+import model.EmployeeType;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+public class EmployeeMapper implements Mapper{
+    @Override
+    public Employee mapRow(ResultSet resultSet) throws SQLException {
+        Employee employe = new Employee();
+        employe.setId(resultSet.getLong("id"));
+        employe.setType(EmployeeType.fromName(resultSet.getString("type")));
+        employe.setFirstName(resultSet.getString("first_name"));
+        employe.setLastName(resultSet.getString("last_name"));
+        employe.setAddress(resultSet.getString("address"));
+        employe.setNickname(resultSet.getString("nickname"));
+        employe.setChief(resultSet.getString("chief"));
+        employe.setHobbies(List.of(resultSet.getString("hobbies").split(",")));
+        employe.setBirthYear(Year.of(resultSet.getInt("birth_year")));
+        employe.setSalary(resultSet.getFloat("salary"));
+        employe.setBonus(resultSet.getFloat("bonus"));
+
+        return employe;
+    }
+
+    @Override
+    public ArrayList<Employee> mapRows(ResultSet resultSet) throws SQLException {
+        ArrayList<Employee> listeEmployes = new ArrayList<>();
+        while(resultSet.next()){
+            listeEmployes.add(mapRow(resultSet));
+        }
+
+        return listeEmployes;
+    }
+}
--- src/mapper/Mapper.java
+++ src/mapper/Mapper.java
@@ -3,10 +3,11 @@ package mapper;
 import model.Employee;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 
 public interface Mapper {
-    Employee mapRow(ResultSet resultSet);
+    Employee mapRow(ResultSet resultSet) throws SQLException;
 
-    Collection<Employee> mapRows(ResultSet resultSet);
+    Collection<Employee> mapRows(ResultSet resultSet) throws SQLException;
 }
