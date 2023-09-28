79497877b31825817a95f6eceee14ca6368dae5e
Matthéo PERELLE <mattheo.perelle@gmail.com>
2023-09-28 08:07:25 +0200

    fix(sql): down sql schema

--- sql/1-init.sql
+++ sql/1-init.sql
@@ -6,8 +6,9 @@ CREATE TABLE IF NOT EXISTS employee
     last_name  VARCHAR(30)                 NOT NULL,
     address    VARCHAR(100)                NOT NULL,
     nickname   VARCHAR(30)                 NOT NULL,
-    chief      INTEGER                              DEFAULT NULL,
-    hobbies    VARCHAR(255)                         DEFAULT NULL,
+    --chief      INTEGER                              DEFAULT NULL,
+    chief        VARCHAR(30)               DEFAULT NULL,
+    hobbies    VARCHAR(255)                DEFAULT NULL,
     birth_year INT                         NOT NULL,
     salary     FLOAT                       NOT NULL,
     bonus      FLOAT                       NOT NULL,
@@ -15,6 +16,6 @@ CREATE TABLE IF NOT EXISTS employee
     CONSTRAINT ck_salary_positive CHECK ( salary > 0 ),
     CONSTRAINT ck_bonus_positive CHECK ( bonus > 0 ),
     CONSTRAINT ck_birth_year CHECK ( birth_year > 0 ),
-    CONSTRAINT fk_chief FOREIGN KEY (chief) REFERENCES employee (id) ON DELETE SET NULL
+    --CONSTRAINT fk_chief FOREIGN KEY (chief) REFERENCES employee (id) ON DELETE SET NULL
 
 );
