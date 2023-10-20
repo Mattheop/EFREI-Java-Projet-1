0710f27102ea55ee15a013632e0c7654bbf4676f
aroullotge1 <aristote.roullot@universite-paris-saclay.fr>
2023-10-20 10:18:49 +0200

    Ajout de la fonctionnalité Recherche

---
+++ src/menu/MenuCommandSearch.java
@@ -0,0 +1,37 @@
+package menu;
+
+import model.Employee;
+import repository.EmployeeRepository;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class MenuCommandSearch implements MenuCommand {
+    private final EmployeeRepository employeeRepository;
+    private final Scanner scanner;
+
+    public MenuCommandSearch(EmployeeRepository employeeRepository, Scanner scanner) {
+        this.employeeRepository = employeeRepository;
+        this.scanner = scanner;
+    }
+
+    public String getCommandName() {
+        return "6. Rechercher un programmeur";
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("Rechercher un programmeur : ");
+        String search = scanner.next();
+        try {
+            ArrayList<Employee> fetchedEmployees = this.employeeRepository.fetchByNames(search);
+            for (Employee employee : fetchedEmployees) {
+                System.out.println(employee);
+            }
+        } catch (SQLException e) {
+            System.out.println("Impossible de communiquer avec la base de données");
+        }
+    }
+}
